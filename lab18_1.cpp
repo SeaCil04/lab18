3759860cd37926515f0b1d64ab38c6fca260e8b4
Seacil04 <everest1756@gmail.com>
2025-02-07 17:13:50 +0700

    lab18

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,37 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
+double overlap(Rect a,Rect b){
+    double sw1,sw2;;
+    if(a.x + a.w <= b.x or b.x + b.w <= a.x) return 0;
+    if(a.y + a.h <= b.y or b.y + b.h <= a.y) return 0;
+    
+    if(a.x + a.w < b.x + b.w) {
+        if(a.x > a.w){
+            sw1 = (a.w + a.w) -a.w;
+        }else{
+            sw1 = (a.x + a.w) - b.x;
+        }
+    }else{
+        if(a.x > b.x){
+            sw1 = (b.x + b.w) - a.x; 
+        }else{
+            sw1 = (b.x + b.w) - b.x;
+        }
+    }
+    if(a.y < b.y){
+        if(a.y - a.h > b.y - b.h){
+            sw2 = a.y - (a.y - a.h);
+        }else{
+            sw2 = a.y - (b.y - b.h);
+        }
+    }else{
+        if(a.y - a.h > b.y - b.h){
+            sw2 = b.y - (a.y - a.h);
+        }else{
+            sw2 = b.y - (b.y - b.h);
+        }
+    }
+    return sw1*sw2;
 
 }
